5ebd70c63fcf25870777f870abbdc5c4599c31d9
Eduardo Gonç <dudue0578@gmail.com>
2023-01-17 13:56:23 -1200

    Controller Delete User

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,6 +6,7 @@ router.post('/', UserController.create);
 router.get('/', UserController.index);
 router.get('/:id', UserController.show);
 router.put('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 export default router;
 
